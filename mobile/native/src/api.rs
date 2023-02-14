5b3231893e5e0ef08d57544d1096b9be92d87d6c
Philipp Hoenisch <philipp@coblox.tech>
2023-02-15 10:15:43 +1100

    dropme: for faster build

--- mobile/native/src/api.rs
+++ mobile/native/src/api.rs
@@ -1,7 +1,1 @@
-use crate::logger;
-use flutter_rust_bridge::StreamSink;
 
-/// Initialise logging infrastructure for Rust
-pub fn init_logging(sink: StreamSink<logger::LogEntry>) {
-    logger::create_log_stream(sink)
-}
